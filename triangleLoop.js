e43c7a2d99e808024b4ec13e498843fd6f11306b
Nitin Reddy <reddynitin3@gmail.com>
2016-10-29 09:21:55 +0530

    Initial commit

---
+++ triangleLoop.js
@@ -0,0 +1,24 @@
+/**
+ * Print Triangle with specific string
+ * @param {number} maxLimit - Number of iterations
+ */
+
+/**
+ * Solution 1
+ */
+function triangleLoop(maxLimit) {
+  var stringChar = "#";
+  for(var count = 1; count < maxLimit; count ++ ) {
+    console.log(stringChar.repeat(count)+"\n");
+  }
+}
+
+
+/**
+ * Solution 2
+ */
+function triangleLoop(maxLimit) {
+ for(var line = '#'; line.length < maxLimit; line += "#" ) {
+    console.log(line);
+  }
+}
